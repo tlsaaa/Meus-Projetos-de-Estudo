1dfaa21ccd6e9859611d994e43e5541fa8ae29f1
abner <abnerthiago128@gmail.com>
2023-08-06 18:23:06 -0300

    add

---
+++ Projeto-5-Copiar_Colar_arq/index.js
@@ -0,0 +1,3 @@
+// essa funcionalidade é apenas para copiar varios arquivos via linha de comando de uma pasta para outra pasta
+// isso será feito em javascript e atraves do node.js
+//o objetivo é apenas didatico para aplicação de novos conhecimentos
